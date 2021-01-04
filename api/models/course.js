815aec8a9917242aab7431d0bb84f7c9ce9ea60d
Nathan Stahlman <nathanstahlman99@gmail.com>
2021-01-03 23:10:06 -0600

    Initial commit
    Add the starter_files from Treehouse and the api I created for project 9.

---
+++ api/models/course.js
@@ -0,0 +1,72 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+// A model for the Courses table in our database.
+module.exports = (sequelize) => {
+    class Course extends Model {}
+    Course.init({
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A title is required.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a title.'
+                }
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A description is required.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a description.'
+                }
+            }
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                notEmpty: {
+                    msg: 'Please provide an estimated time.'
+                }
+            }
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                notEmpty: {
+                    msg: 'Please provide the materials needed.'
+                }
+            }
+        }
+    }, { sequelize });
+
+    // Creates a relationship with the Users table in our database, and adds the foreign key as the value 'userId'.
+    Course.associate = (models) => {
+        Course.belongsTo(models.User, { foreignKey: 'userId' });
+      };
+
+return Course;
+};
+
+(async () => {
+    try {
+      await Course.sync();
+      console.log('Course table synced successfully.');
+    } catch (error) {
+      console.error('Unable to sync course table successfully:', error);
+    }
+  });
---
+++ api/routes.js
@@ -0,0 +1,137 @@
+'use strict';
+
+// Import modules.
+const express = require('express');
+const router = express.Router();
+const { asyncHandler } = require('./middleware/async-handler');
+const { authenticateUser } = require('./middleware/auth-user');
+const User = require('./models').User;
+const Course = require('./models').Course;
+
+/*
+  User Routes
+*/
+
+// A GET route that retrieves the current user.
+router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+    const user = req.currentUser;
+
+    res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        emailAddress: user.emailAddress
+    });
+}));
+
+// A POST route that creates (POSTs) a user to the database.
+router.post('/users', asyncHandler(async (req, res) => {
+    try {
+        await User.create(req.body);
+        res.location('/');
+        res.status(201).send();
+      } catch (error) {
+        console.log('ERROR: ', error.name);
+    
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+          const errors = error.errors.map(err => err.message);
+          res.status(400).json({ errors });   
+        } else {
+          throw error;
+        }
+      }
+}));
+
+/*
+  Course Routes
+*/
+
+// A GET route that retrieves the list of courses.
+router.get('/courses', asyncHandler(async (req, res) => {
+  const courses = await Course.findAll({
+    attributes: {exclude: ['createdAt', 'updatedAt', 'userId']},
+    include: [{
+      model: User,
+      attributes: {exclude: ['createdAt', 'updatedAt', 'password']}
+    }]
+  });
+
+  res.json({
+      courses
+  });
+}));
+
+// A GET route that retrieves a specific course.
+router.get('/courses/:id', asyncHandler(async (req, res) => {
+  const course = await Course.findByPk(req.params.id, {
+    attributes: {exclude: ['createdAt', 'updatedAt', 'userId']},
+    include: [{
+      model: User,
+      attributes: {exclude: ['createdAt', 'updatedAt', 'password']}
+    }]
+  });
+
+  res.json({
+      course
+  });
+}));
+
+// A POST route that creates (POSTs) a course to the database.
+router.post('/courses', authenticateUser, asyncHandler(async (req, res) => {
+  try {
+      const course = await Course.create(req.body);
+      res.location(`/courses/${course.id}`);
+      res.status(201).send();
+    } catch (error) {
+      console.log('ERROR: ', error.name);
+  
+      if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+        const errors = error.errors.map(err => err.message);
+        res.status(400).json({ errors });   
+      } else {
+        throw error;
+      }
+    }
+}));
+
+// A PUT route that allows the users to update a specific course.
+router.put('/courses/:id', authenticateUser, asyncHandler(async(req, res) => {
+
+  let course;
+  try {
+    course = await Course.findByPk(req.params.id);
+    if(course) {
+      if (req.currentUser.id == course.userId) {
+        await course.update(req.body);
+        res.status(204).send();
+      } else {
+        res.status(403).json({ Forbidden: "Unauthorized User" });
+      }
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      const errors = error.errors.map(err => err.message);
+      res.status(400).json({ errors });
+    } else {
+      throw error;
+    }
+  }
+}));
+
+// A DELETE route that deletes a specific course.
+router.delete('/courses/:id', authenticateUser, asyncHandler(async(req, res) => {
+  const course = await Course.findByPk(req.params.id);
+  if(course) {
+    if (req.currentUser.id == course.userId) {
+      await course.destroy();
+      res.status(204).send();
+    } else {
+      res.status(403).json({ Forbidden: "Unauthorized User" });
+    }
+  } else {
+    res.status(404).send();
+  }
+}));
+
+module.exports = router;
