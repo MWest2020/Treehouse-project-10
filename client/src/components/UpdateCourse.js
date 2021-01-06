2ac33243e7df4defac240a31e6e0a637ca2b1151
Nathan Stahlman <nathanstahlman99@gmail.com>
2021-01-06 14:25:37 -0600

    Update the Header to change based on logged in users.

---
+++ client/src/components/UpdateCourse.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+export default function UpdateCourse (props) {
+    
+}
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Header () {
+export default function Header (props) {
     return (
         <div className="header">
             <div className="bounds">
                 <h1 className="header--logo">Courses</h1>
                 <nav>
-                    <Link to="/signup" className="signup">Sign Up</Link>
-                    <Link to="/signin" className="signin">Sign In</Link>
+                    {props.authenticatedUser ? (
+                        <React.Fragment>
+                            <span>Welcome, {props.authenticatedUser.firstName + " " + props.authenticatedUser.lastName}!</span>
+                            <Link to="/signout">Sign Out</Link>
+                        </React.Fragment>
+                    ) : (
+                        <React.Fragment>
+                            <Link to="/signup" className="signup">Sign Up</Link>
+                            <Link to="/signin" className="signin">Sign In</Link>
+                        </React.Fragment>
+                    )}
                 </nav>
             </div>
         </div>
